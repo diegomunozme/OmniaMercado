760c6e97876956c930c0c54e7898b6faecfb93a6
Diego Munoz <70450173+diegomunozme@users.noreply.github.com>
2022-06-07 15:54:06 -0700

    Beginning landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import classes from "./styles.css";
+import styled from "styled-components";
 
-export default function App() {
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  font-size: 20px;
+  padding: 10px 60px;
+  border-radius: 5px;
+  margin: 10px 0px;
+  cursor: pointer;
+`;
+
+const App = () => {
   return (
     <div className={classes.App}>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Welcome to OmniaMercado</h1>
+      <h2>We'll hedge it</h2>
+      <Button>LesGetIt</Button>
+      <Button>LesGetIt</Button>
     </div>
   );
-}
+};
+
+export default App;
